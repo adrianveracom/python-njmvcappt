624d7ca895189b5e72733d70fc2287c1994941ea
Adrian Vera <1236638+adrianveracom@users.noreply.github.com>
2021-11-01 18:40:41 -0400

    Add audio

--- dl-alert.py
+++ dl-alert.py
@@ -3,21 +3,15 @@ import re
 import time
 from bs4 import BeautifulSoup
 from datetime import datetime
-import winsound
+from playsound import playsound
 
 location_arr = ['101','102','103','104','105','106','107','108','109','110','111','112','113','114','115','116','117','118','119','120','121','122','123']
 locationname_arr = ['Lawrenceville','Bayonne','North Cape May','Camden','Cardiff','Salem','Delanco','Eatontown','SouthPlainfield','Edison','Flemington','Toms River','Freehold','Lodi','Vineland','Newark','North Bergen','Wayne','Oakland','Paterson','Thorofare','Rahway','Randolph']
 base_url_link='https://telegov.njportal.com/njmvc/AppointmentWizard/11/'
-required_months = ['May','June']
+required_months = ['October','November', 'December', 'January']
 
 def beep():
-    winsound.Beep(1500, 500)
-    winsound.Beep(4500, 500)
-    winsound.Beep(2500, 500)
-    winsound.Beep(1500, 500)
-    winsound.Beep(4500, 500)
-    winsound.Beep(2500, 500)
-
+    playsound('audio.mp3')
 
 def job():
     now = datetime.now()
@@ -26,33 +20,37 @@ def job():
     i=0
     found=0
     
-    
     for location in location_arr:
-        print(location)
-        with urllib.request.urlopen(base_url_link+location) as response:
+        print(location + locationname_arr[i])
+        with urllib.request.urlopen(base_url_link + location) as response:
             page_html = response.read()
         soup = BeautifulSoup(page_html ,'lxml')
-        unavailable=soup.find('div',attrs={'class': 'alert-danger'})
+        unavailable = soup.find('div',attrs={'class': 'alert-danger'})
+
         if unavailable is not None :
-            #print('No appointments are available in '+locationname_arr[i])
+            print('No appointments are available in '+locationname_arr[i])
             dt_string=""
         else:
             dates_html = soup.find('div',attrs={'class': 'col-md-8'})
-            date_string = dates_html.find('label',attrs={'class': 'control-label'})
-            if set(required_months) & set(date_string.text.split()):
-                #print("Matching required months")
-                date_string=re.sub('Time of Appointment for ', '', date_string.text)
-                date_string=re.sub(':', '', date_string)
-                message = 'DL Renew Dates: '+locationname_arr[i]+' / ('+location+') : '+date_string
-                print(message)
-                beep()
-                found=1
+            if dates_html is not None :
+                date_string = dates_html.find('label',attrs={'class': 'control-label'})
+                if set(required_months) & set(date_string.text.split()):
+                    date_string=re.sub('Time of Appointment for ', '', date_string.text)
+                    date_string=re.sub(':', '', date_string)
+                    message = 'Knowledge test date: '+locationname_arr[i]+' / ('+location+') : '+date_string
+                    print(message)
+                    found=1
         i=i+1
+    
+    if found:
+        print('Appointment found')
+        beep()
         
 while True :
     try:
         job()
-    except:
+    except Exception as e: 
+        print(e)
         print("Something went wrong")
         time.sleep(60)
     else:
